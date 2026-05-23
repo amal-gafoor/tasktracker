5e7f9e6efbe13ffc91889cd22c80bdfffaf401f8
Amal Gafoor P K <amalabdulgafoor9557@gmail.com>
2026-05-24 02:49:03 +0530

    debug

--- backend/src/controller/task_controller.py
+++ backend/src/controller/task_controller.py
@@ -1,40 +1,39 @@
 from src.config.database import get_db
 from src.models.schemas import CreateTask, TaskUpdate
 from sqlalchemy.orm import Session
-from fastapi import Depends
 from src.service.task_service import create_task, delete_task, get_completed_by_range,get_task_by_id,get_all_task, update_task
 
 def create_task_controller(
         task: CreateTask,
-        db: Session = Depends(get_db)):
+    db: Session):
     return create_task(db,task)
 
 def get_task_controller(
         task_id:int,
-        db:Session = Depends(get_db)
+    db:Session
 ):
     return get_task_by_id(db,task_id)
 
 def get_all_task_controller(
-        db:Session = Depends(get_db)
+    db:Session
 ):
     return get_all_task(db)
 
 def update_task_controller(
         task_id:int,
         update_data:TaskUpdate,
-        db:Session = Depends(get_db)
+    db:Session
 ):
     return update_task(db,task_id,update_data)
 
 def delete_task_controller(
         task_id:int,
-        db:Session = Depends(get_db)
+    db:Session
 ):
     return delete_task(db,task_id)
 
 def get_completed_by_range_controller(
         range:str,
-        db:Session = Depends(get_db)
+    db:Session
 ):
     return get_completed_by_range(db,range)
--- backend/src/route/task_route.py
+++ backend/src/route/task_route.py
@@ -5,30 +5,32 @@ from src.controller.task_controller import (
     get_all_task_controller, get_task_controller,
     update_task_controller)
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from src.config.database import get_db
 from src.models.schemas import CreateTask, TaskUpdate, TaskResponse, CompleteTaskResponse
 
 router = APIRouter(prefix='/tasks', tags=["tasks"])
 
 @router.get("/completed", response_model=CompleteTaskResponse)
-def get_completed_by_range(range:str):
-    return get_completed_by_range_controller(range)
+def get_completed_by_range(range:str, db: Session = Depends(get_db)):
+    return get_completed_by_range_controller(range, db)
 
 @router.post("")
-def create_task(task_data:CreateTask):
-    return create_task_controller(task_data)
+def create_task(task_data:CreateTask, db: Session = Depends(get_db)):
+    return create_task_controller(task_data, db)
 
-@router.delete("{task_id}")
-def delete_task(task_id:int):
-    return delete_task_controller(task_id)
+@router.delete("/{task_id}")
+def delete_task(task_id:int, db: Session = Depends(get_db)):
+    return delete_task_controller(task_id, db)
 
 @router.get("")
-def get_all_tasks():
-    return get_all_task_controller
+def get_all_tasks(db: Session = Depends(get_db)):
+    return get_all_task_controller(db)
 
 @router.get("/{task_id}")
-def get_task(task_id=int):
-    return get_task_controller(task_id)
+def get_task(task_id:int, db: Session = Depends(get_db)):
+    return get_task_controller(task_id, db)
 
-@router.put("{task_id}")
-def update_task(task_id:int, update_data:TaskUpdate):
-    return update_task_controller(task_id,update_data)
+@router.put("/{task_id}")
+def update_task(task_id:int, update_data:TaskUpdate, db: Session = Depends(get_db)):
+    return update_task_controller(task_id,update_data, db)
